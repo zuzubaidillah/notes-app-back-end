92f91445d7cadb368be144285ca6c2f456ea84ca
zuzubaidillah <zuzubaidillah@gmail.com>
2021-08-08 08:26:37 +0700

    firstUpload

--- src/server.js
+++ src/server.js
@@ -1,17 +1,21 @@
-// console.log('Hallo kita akan membuat RESTful API');
 const Hapi = require('@hapi/hapi');
-
+const routes = require('./routes');
 
 const init = async () => {
-    const server = Hapi.server({
-        port: 5000,
-        host: 'localhost',
-    });
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
 
+  server.route(routes);
 
-    await server.start();
-    console.log(`Server berjalan pada ${server.info.uri}`);
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
-
 
 init();
