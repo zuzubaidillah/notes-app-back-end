7dd8817e19f4495850cd3ae9b0a082994a60277c
zuzubaidillah <zuzubaidillah@gmail.com>
2021-08-08 09:22:43 +0700

    isi notes

--- src/notes.js
+++ src/notes.js
@@ -1,3 +1,44 @@
-const notes = [];
+const notes = [
+  {
+    title: 'AWS dicoding',
+    tags: 'belajar',
+    body: 'deskripsi',
+    id: '1',
+    createdAt: '2021-08-08',
+    updatedAt: '2021-08-09',
+  },
+  {
+    title: 'ocalhost',
+    tags: 'Learning',
+    body: 'desk',
+    id: '2',
+    createdAt: '2021-08-08',
+    updatedAt: '2021-08-09',
+  },
+  {
+    title: 'Tabnine',
+    tags: 'Vs Code',
+    body: 'Deskr',
+    id: '3',
+    createdAt: '2021-08-08',
+    updatedAt: '2021-08-09',
+  },
+  {
+    title: 'Gold Medal',
+    tags: 'Indonesia Meraih Emas',
+    body: 'Deskripsi',
+    id: '4',
+    createdAt: '2021-08-08',
+    updatedAt: '2021-08-09',
+  },
+  {
+    title: 'Berita Bom Indonesia',
+    tags: 'ISIS',
+    body: 'Deskripsi',
+    id: '5',
+    createdAt: '2021-08-08',
+    updatedAt: '2021-08-09',
+  },
+];
 
 module.exports = notes;
